6c94fc9813313eafca87a43b96d199aa344e265b
Jacob <103798568+j-tad@users.noreply.github.com>
2023-07-16 20:58:00 -0400

    Add calendar component (#1)

--- src/apps/schedule/components/Calendar/Calendar.svelte
+++ src/apps/schedule/components/Calendar/Calendar.svelte
@@ -1,27 +1,44 @@
 <script>
   import "../Calendar/Calendar.style.scss";
-
   const DAYS = 7;
-  const BLOCKS = 24;
+  const BLOCKS = 29;
+  const PAST12 = 8;
+  const START_TIME = 8;
+  let militaryTime = false;
+  const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 </script>
 
-<div class="calendar">
-  {#each Array(DAYS) as _, i}
-    <div>
-      <div>Day: {i}</div>
-      <div class="calendar-day">
-        {#each Array(BLOCKS) as _, j}
-          {#if j % 2 === 0}
-            <div class="calendar-time-blocks">
-              Block: {j} (even)
-            </div>
-          {:else}
-            <div class="calendar-time-blocks">
-              Block: {j} (odd)
-            </div>
+<div class="calendar-container">
+  <div class="calendar">
+    {#each Array(DAYS + 1) as _, i}
+      <div>
+        <div class="day-label">
+          {#if i !== 0}
+            {weekdays[i - 1]}
           {/if}
-        {/each}
+        </div>
+        <div class="calendar-day">
+          {#each Array(BLOCKS) as _, j}
+            {#if i !== 0}
+              <div class="calendar-time-blocks outline" />
+            {:else if j % 2 == 0}
+              <div class="calendar-time-blocks">
+                {#if militaryTime === false && j > PAST12}
+                  <div>{Math.floor(j / 2) + START_TIME - 12}:00</div>
+                {:else}
+                  <div>{Math.floor(j / 2) + START_TIME}:00</div>
+                {/if}
+              </div>
+            {:else}
+              <div class="calendar-time-blocks" />
+            {/if}
+          {/each}
+        </div>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </div>
+
+<button on:click={() => (militaryTime = !militaryTime)}
+  >Toggle Military Time</button
+>
